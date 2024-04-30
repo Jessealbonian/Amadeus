ff375812396e76c2e6bf4f1a207df3d2e52d1b6e
Jessealbonian <157551756+Jessealbonian@users.noreply.github.com>
2024-04-30 08:06:13 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,102 @@
+<php>
+<!DOCTYPE html>
+<html>
+<head>
+<title>W3.CSS Template</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="style.css">
+<link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+<style>
+html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif}
+</style>
+</head>
+<body class="light-grey">
+
+<!-- Page Container -->
+<div class="content w3-margin-top" style="max-width:1400px;">
+
+  <!-- The Grid -->
+  <div class="w3-row-padding">
+  
+    <!-- Left Column -->
+    <div class="w3-third">
+    
+      <div class="cont1">
+        <div class="display-container">
+          <img src="./Asset/Logo1.jpg" style="width:400px" alt="Appplication Logo">
+          <div class="display-bottomleft container text-black">
+            <h2></h2>
+          </div>
+        </div>
+        <div class="w3-container">
+          <br>
+        </div>
+      </div><br>
+    <!-- End Left Column -->
+    </div>
+
+    <!-- Right Column -->
+    <div class="w3-twothird">
+        <div class="logocenter">
+            <img src="./Asset/Logo1.jpg" style="width:200px" alt="Appplication Logo">
+            <h2>Welcome to PACC</h2>
+            <form action="/action_page.php"><!-- post function -->
+              <label for="pwd">Student Number:</label><br>
+                <input type="Number" id="fname" name="fname" value="Student Number: "><br>
+            </form>
+        </div>
+      <div class="w3-container w3-card w3-white w3-margin-bottom">
+         <!-- 2nd Container --> 
+         <!-- Get Function -->
+         <table>
+            <th class = tabhed>
+                <th>Time in</th>
+                <th>Student Number</th>
+                <th>Status</th>
+            </th>
+            <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+            <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+            <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+            <tr>
+                <th></th>
+                <th></th>
+                <th></th>
+            </tr>
+
+
+            </table>
+
+        <div class="w3-container">
+
+      </div>
+
+      <div class="w3-container w3-card w3-white"></div>
+
+    <!-- End Right Column -->
+    </div>
+    
+  <!-- End Grid -->
+  </div>
+  
+  <!-- End Page Container -->
+</div>
+
+</body>
+</html>
+
+
+
+</php>
